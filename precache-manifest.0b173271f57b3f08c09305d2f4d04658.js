2c0a1a525c4047905f168925fa62e813d5f9c221
Hasija <sankalpinfotech96@gmail.com>
2020-09-22 22:18:40 +0530

    first commit

---
+++ precache-manifest.0b173271f57b3f08c09305d2f4d04658.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c291b16169355b77eb282b57e272c6a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1a7086ae185aa619e8b",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "087b8eb6c6e3e632e9ea",
+    "url": "/static/css/main.a5c76850.chunk.css"
+  },
+  {
+    "revision": "e1a7086ae185aa619e8b",
+    "url": "/static/js/2.92e3eed9.chunk.js"
+  },
+  {
+    "revision": "aac53d728dbff1a824861db4bf948402",
+    "url": "/static/js/2.92e3eed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "087b8eb6c6e3e632e9ea",
+    "url": "/static/js/main.127f5718.chunk.js"
+  },
+  {
+    "revision": "b3d21c6cf7515b261f12e2e0d77b0dbb",
+    "url": "/static/js/main.127f5718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
